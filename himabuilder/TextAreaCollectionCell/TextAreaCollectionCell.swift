b2d487be731dbe25ad48f212d490a7aac6a4a365
alberto <alberto.paladino@dualb.it>
2022-10-14 16:32:15 +0200

    fixed mandatory textarea

--- himabuilder/TextAreaCollectionCell/TextAreaCollectionCell.swift
+++ himabuilder/TextAreaCollectionCell/TextAreaCollectionCell.swift
@@ -34,15 +34,23 @@ class TextAreaCollectionCell: UICollectionViewCell, UITextViewDelegate {
     
     func setup() {
         self.lblTitle.text = data.title
-        self.txtValue.text = data.value
+        if data.value == "" {
+            self.txtValue.text = "Tocca qui per scrivere"
+            self.txtValue.textColor = .systemGray
+        } else {
+            self.txtValue.text = data.value
+        }
+        if !data.editable {
+            self.txtValue.text = data.value
+            self.txtValue.textColor = .systemGray
+        }
         self.txtValue.isUserInteractionEnabled = data.editable
-        self.txtValue.textColor = data.editable ? .label : .systemGray
-        self.txtValue.delegate = self        
+        self.txtValue.delegate = self
         evaluateMandatory()
     }
     
     func evaluateMandatory() {
-        if data.mandatory && data.value == nil {
+        if data.mandatory && data.value?.isEmpty ?? true {
             self.lblTitle.textColor = .red
         } else {
             self.lblTitle.textColor = .label
@@ -50,6 +58,7 @@ class TextAreaCollectionCell: UICollectionViewCell, UITextViewDelegate {
     }
     
     func textViewDidBeginEditing(_ textView: UITextView) {
+        if !data.editable { return }
         if textView.textColor == UIColor.systemGray {
             textView.text = nil
             textView.textColor = UIColor.label
@@ -60,6 +69,11 @@ class TextAreaCollectionCell: UICollectionViewCell, UITextViewDelegate {
         if textView.text.isEmpty {
             textView.text = "Tocca qui per scrivere"
             textView.textColor = UIColor.systemGray
+            if data.mandatory {
+                self.lblTitle.textColor = .red
+            }
+        } else {
+            self.lblTitle.textColor = .label
         }
     }
     
